4209e5f433392a8c5560d554e14e854e13264a19
valdeveloper <jjavaldz@gmail.com>
2023-06-26 18:20:22 -0400

    endpoints + controllers: categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
+import read from '../controllers/categories/read.js'
 
 const category_router = Router()
 
 //category_router.post()       //POST: para crear un category
-//category_router.get()        //GET: para leer (TODOS o SOLO UNO) categoryes
+category_router.get('/',read)        //GET: para leer (TODOS o SOLO UNO) categoryes
 //category_router.put()        //PUT: para actualizar un category
 //category_router.delete()     //DELETE: para eliminar un category
 
